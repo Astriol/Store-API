e8f6ce2965abeaff5c469475edba1d9e7e823ec6
Paige Mundy <paigemundy@Paiges-MacBook-Pro.local>
2023-01-12 16:24:48 -0600

    last commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,13 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) =>{
-  const search = 'ab'
-  const products = await Product.find({
-    name:{$regex: search, $options: 'i'},
-  })
+  const products = await Product.find({price:{$gt: 30}}).sort('price').select('name price')
+
   res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async (req, res) =>{
-  const {featured, company, name} = req.query
+  const {featured, company, name, sort, fields, numericFilters} = req.query
 const queryObject = {}
 
 if(featured){
@@ -21,8 +19,48 @@ if(company){
 if (name) {
   queryObject.name = {$regex: name, $options: 'i'}
 }
-console.log(queryObject);
-  const products = await Product.find(queryObject);
+//console.log(queryObject);
+if(fields){
+  const fieldsList = fields.split(',').join(' ');
+  result = result.select(fieldsList)
+}
+
+if (numericFilters) {
+  const operatorMap = {
+    '>': '$gt',
+    '>=': '$gte',
+    '=': '$eq',
+    '<': '$lt',
+    '<=': '$lte',
+  };
+  const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+  let filters = numericFilters.replace(
+    regEx,
+    (match) => `-${operatorMap[match]}-`
+  );
+  const options = ['price', 'rating'];
+  filters = filters.split(',').forEach((item) => {
+    const [field, operator, value] = item.split('-');
+    if (options.includes(field)) {
+      queryObject[field] = { [operator]: Number(value) };
+    }
+  });
+}
+if(sort){
+  const sortList = sort.split(',').join(' ');
+  result = result.sort(sortList)
+}
+
+  let result = Product.find(queryObject)
+
+//sort
+ const page = Number(req.query.page) || 1
+ const limit = Number(req.query.limit) || 10
+ const skip = (page -1) * limit;
+
+ result = result.skip(skip).limit(limit)
+
+  const products = await result
   res.status(200).json({products, nbHits:products.length})
 }
 
